9af44f1a92ad62b3014b15f8d13fd6ee2386c4d9
Maan.x <163430511+tandelmaan@users.noreply.github.com>
2026-08-24 05:44:00 +0000

    Add Loops in JS

---
+++ intermediate/loops.js
@@ -0,0 +1,67 @@
+// For Loops 
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("maan " + i)
+// }
+
+// let coding = ["Js", "Python", "Cpp"]
+// console.log(coding[0])
+// for (let i = 0; i < coding.length; i++) {
+//     console.log(coding[i])
+// }
+
+// let teamMember = ["Maan", "Neel", "Raj"]
+// for (let i = 0; i < teamMember.length; i++) {
+//     console.log(`Team ${i} : ${teamMember[i]}`)
+//     for (j = 1; j <= 3; j++) {
+//         console.log(`Member ${j}`)
+//     }
+// }
+
+
+// // While loops 
+// let i = 1
+// while (i <= 10) {
+//     console.log("bajate raho : " + i)
+//     i++
+// }
+
+// // * (astrict Exanmple)
+// for (let i = 1; i <= 5; i++) {
+//     if (i == 1) {
+//         console.log("*")
+//     }
+//     else if (i == 2) {
+//         console.log("**")
+//     }
+//     else if (i == 3) {
+//         console.log("***")
+//     }
+//     else if (i == 4) {
+//         console.log("****")
+//     }
+//     else {
+//         console.log("*****")
+//     }
+
+// }
+// for (let i = 1; i <= 5; i++) {
+//     console.log("*".repeat(i));
+// }
+
+// const obj1 = {
+//     name: "maan",
+//     age: 12,
+//     address: {
+//         city: "nvs",
+//         complex: "B"
+//     }
+// }
+// const fruits = ["avacado", "mango", "jambu", "banana", "orange"]
+// for (fru of fruits) {
+//     // console.log(fru)
+// }
+// for (info in obj1) {
+//     console.log(info + " : " + obj1[info])
+//     console.log(obj1[info])
+// }
